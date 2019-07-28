a769e63de0e077c4040155c49c51186573724e7b
Colin <cstatro@gmail.com>
2019-07-28 11:06:29 -0400

    put in a test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Should show user" do 
+    
+    x = User.all.length
+
+    assert_equal 2,x,"User creation works"
+    User.destroy_all
+  end
+
 end
