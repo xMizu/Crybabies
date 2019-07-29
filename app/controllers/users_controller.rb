f849b864fbfdc0e8d171b4a31b42a1cb1bdfff7f
Colin <cstatro@gmail.com>
2019-07-29 10:03:29 -0400

    made controller method for rounds/users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,28 @@
 class UsersController < ApplicationController
+    def new
+        @user = User.new 
+    end
+    def show
+        @user = User.find(params[:id])
+    end
+    def delete
+        @user = User.find(params[:id])
+        @user.delete 
+        redirect_to root_path 
+    end
+    def create
+        @user = User.new(user_params)
+        if @user.valid?
+            @user.save 
+            redirect_to root_path
+        else 
+            #TODO need to handle invalid user
+        end
+    end
+
+
+    private 
+    def user_params
+        params.require(:user).permit(:name,:email)
+    end
 end
