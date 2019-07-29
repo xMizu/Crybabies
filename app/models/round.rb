13cc0f3a3d352dfe95fc5b9c1bb894b321ed64b0
Colin <cstatro@gmail.com>
2019-07-29 14:22:49 -0400

    added tests and helpers for round

--- app/models/round.rb
+++ app/models/round.rb
@@ -3,5 +3,34 @@ class Round < ApplicationRecord
     belongs_to :topic 
     has_many :complaints
     has_many :guesses
-    
+
+    def player_count
+        self.game.user_count
+    end
+    def number_of_expected_guesses
+        self.player_count*(self.player_count-1)
+    end 
+
+    def complaint_phase?
+        if self.complaints.length < player_count
+            true
+        else
+            false
+        end
+    end
+
+    def guess_phase?  
+        if self.guesses.length < self.number_of_expected_guesses
+            true 
+        else
+            false 
+        end
+    end
+    def finished?
+        if  !guess_phase? && !complaint_phase?
+            true
+        else
+            false
+        end
+    end
 end
--- test/controllers/complaints_controller_test.rb
+++ test/controllers/complaints_controller_test.rb
@@ -1,14 +1,14 @@
 require 'test_helper'
 
 class ComplaintsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get complaints_new_url
-    assert_response :success
-  end
+  # test "should get new" do
+  #   get complaints_new_url
+  #   assert_response :success
+  # end
 
-  test "should get create" do
-    get complaints_create_url
-    assert_response :success
-  end
+  # test "should get create" do
+  #   get complaints_create_url
+  #   assert_response :success
+  # end
 
 end
