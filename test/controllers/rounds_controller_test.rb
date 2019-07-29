e81736d1fcf72cadb530e9406611d66410c0b30d
Colin <cstatro@gmail.com>
2019-07-29 17:42:09 -0400

    added rounds

--- test/controllers/rounds_controller_test.rb
+++ test/controllers/rounds_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class RoundControllerTest < ActionDispatch::IntegrationTest
+class RoundsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
