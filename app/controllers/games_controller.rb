e586bdb95d476784d0f2491ad24ad26379af0dd6
Anthony <anthonywan00@gmail.com>
2019-07-29 14:31:58 -0400

    added new game button

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,21 +1,30 @@
 class GamesController < ApplicationController
     def show
         @game = Game.find(params[:id])
+        @players = GameSession.find_by(game_id: @game.id)
     end
     def new
         @game = Game.new 
-        
     end
     def create
         #TODO figure out how to make a game session for the current user. 
         @game = Game.new(game_params)
         if @game.valid?
             @game.save
+\            redirect_to @game
         else 
             flash.now[:message] = @game.errors.full_messages[0]
             render :new 
         end
     end
+
+    def started?
+        @game = game.find(parms[:id])
+    end
+
+
+
+
     private 
     def game_params
         params.require(:game).permit(:user_count,:max_rounds)
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,25 @@
 Rails.application.routes.draw do
+  root 'sessions#new'
+  
+  #session routes
+  get "/login", to: "sessions#new", as: "login"
+  post "/sessions", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+  
+  #user routes
+  resources :users, only: [:delete,:show,:create,:new]
+  get '/signup/', to: 'users#new', as: 'signup'
+  post '/login/', to: 'sessions#create' 
+  
+  resources :games, only: [:show,:create,:new]
+  
+
+
   resources :topics , only: [:index,:show]
   resources :guesses , only: [:new,:create]
   resources :complaints , only: [:new,:create]
   resources :game_sessions , only: [:create, :update,:show]
-  root 'sessions#new'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/login", to: "sessions#new", as: "login"
-  post "/login", to: "sessions#create"
-  delete "/logout", to: "sessions#destroy"
-
-  resources :users, only: [:delete,:show,:create]
-  get '/signup', to: "users#new", as: "signup"
   resources :rounds, only: [:create,:update]
-  resources :games, only: [:show,:create,:new]
   
 
 
