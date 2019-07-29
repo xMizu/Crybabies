d8211a3804fd2be8439c669c0ad7cc1baa33a4d4
Colin <cstatro@gmail.com>
2019-07-29 15:26:07 -0400

    added helper to determine if guess is correct

--- app/models/guess.rb
+++ app/models/guess.rb
@@ -2,4 +2,12 @@ class Guess < ApplicationRecord
     belongs_to :user
     belongs_to :round 
     belongs_to :complaint
+
+    def correct_guess?
+        if self.guess == self.complaint.user.id
+            true
+        else 
+            false
+        end
+    end
 end
--- test/models/guess_test.rb
+++ test/models/guess_test.rb
@@ -4,4 +4,36 @@ class GuessTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "correct guess returns true if correct" do 
+    u1 = User.create(name:"Jacob")
+    u2 = User.create(name:"Birdy")
+    u3 = User.create(name:"Jeb")
+    g =Game.create(user_count:3,max_rounds:2)
+    s1 = GameSession.create(user: u1, game:g)
+    s2 = GameSession.create(user: u2, game:g)
+    s3 = GameSession.create(user: u2, game:g)
+    t = Topic.create(name:"Anime")
+    r = Round.create(topic: t, game: g)
+    c1 = Complaint.create(complain_text:"Not realistic",user:u1, round: r)
+    c2 = Complaint.create(complain_text:"Not Strange Enough",user:u2, round: r)
+    c3 = Complaint.create(complain_text:"I can't read",user:u3, round: r)
+    g1 = Guess.create(complaint: c2, user: u1,guess:u2.id,round:r)
+    assert g1.correct_guess? , "false negative on correct guess"
+  end
+  test "correct guess returns false if incorrect" do 
+    u1 = users(:one)
+    u2 = users(:two)
+    u3 = User.create(name:"Jeb")
+    g =Game.create(user_count:3,max_rounds:2)
+    s1 = GameSession.create(user: u1, game:g)
+    s2 = GameSession.create(user: u2, game:g)
+    s3 = GameSession.create(user: u2, game:g)
+    t = Topic.create(name:"Anime")
+    r = Round.create(topic: t, game: g)
+    c1 = Complaint.create(complain_text:"Not realistic",user:u1, round: r)
+    c2 = Complaint.create(complain_text:"Not Strange Enough",user:u2, round: r)
+    c3 = Complaint.create(complain_text:"I can't read",user:u3, round: r)
+    g1 = Guess.create(complaint: c2, user: u1,guess:u1.id,round:r)
+    assert_not g1.correct_guess? , "false positive on incorrect guess"
+  end
 end
