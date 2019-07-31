5f7dc7989a81139ad6918e23a8ddee9a95e258ae
Anthony <anthonywan00@gmail.com>
2019-07-31 11:25:17 -0400

    added to slug schema

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+    before_action :authorized?, except: [:new,:create]
+
     def new
         @user = User.new 
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    after_validation :set_slug, only: [:create, :update]
     has_many :game_sessions
     has_many :games, through: :game_sessions
     has_many :guesses
@@ -18,6 +19,10 @@ class User < ApplicationRecord
         end
     end
 
+    def to_param
+        "#{id}-#{slug}"
+      end
+
     def current_round?
         if self.current_session? == true && self.games.last.rounds.empty? 
             self.games.last
@@ -30,6 +35,12 @@ class User < ApplicationRecord
     def score 
         current_session.score
     end
+
+    private
+
+    def set_slug
+    self.slug = name.parameterize
+    end 
 
     
 end
