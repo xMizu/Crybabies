b2a448c622e8505ca2183f8594445a68fc5257f1
Anthony <anthonywan00@gmail.com>
2019-08-01 13:53:27 -0400

    jeff help

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_07_31_150904) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "complaints", force: :cascade do |t|
     t.string "complain_text"
     t.integer "round_id"
